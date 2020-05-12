8e5468451e3399e9fb17ef4044c8e91919ca0816
Saif161114 <48291889+Saif161114@users.noreply.github.com>
2020-05-12 12:15:14 +0500

    Add files via upload

---
+++ Driver.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+class TopicDetection(object):
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    def compute_something(self):
+        pdf = extract_text_from_pdf(self.input_file)
+
+        print('\nPDF contains {} words'.format(len(pdf)))
+
+        print('\nTen most frequent words are:\n{}'.format(get_10_most_frequent_words(pdf)))
+
+        print('\nWords longer than 15 characters:\n{}'.format(find_long_words(pdf)))
+
+        print('\nThe topic may be familiar with those words:\n{}'.format(find_long_and_common_words(pdf)))
+
+td = TopicDetection(input_file="sample1.pdf")
+td.compute_something()
